cc1a29a8d7958d285d02ad7f28b6c62c825352e6
James Daniel Johnson <jdjohnson8883@gmail.com>
2023-02-22 13:59:09 -0500

    First functional prototype of csv example

--- src/main/java/edu/cofc/johnson/Example.java
+++ src/main/java/edu/cofc/johnson/Example.java
@@ -1,15 +1,27 @@
 package edu.cofc.johnson;
+//  AUTHOR: James Daniel Johnson
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.ByteBuffer;
-public class Example  {
-    public static final String FILE_PATH = "./example.csv";
-    private int id;
-    private float data;
-    private String name;
-    private char symbol;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Optional;
 
+/**
+ *  This is class serves as an example of writing a class to a file.
+ *  It features 2 static methods: one to write an object to a file, and
+ *  another to read an array of objects from a file.
+ */
+public class Example  {
+    private static final int NUMBER_OF_FIELDS = 4;
+    private static final int ID_INDEX = 0;
+    private static final int DATA_INDEX = 1;
+    private static final int NAME_INDEX = 2;
+    private static final int SYMBOL_INDEX = 3;
+    private final int id;
+    private final float data;
+    private final String name;
+    private final char symbol;
     public Example(int id,
                    float data,
                    String name,
@@ -19,16 +31,7 @@ public class Example  {
         this.name = name;
         this.symbol = symbol;
     }
-    public static void main(String[] args) {
-        Example myExample = new Example(0,
-                0.1234f,
-                "myExample",
-                'G');
-        System.out.printf("The example class we made is myExample{%s}\n", myExample);
-
-
-    }
-    // Getters
+   // Getters
     public int getId() {
         return id;
     }
@@ -45,25 +48,73 @@ public class Example  {
         return name;
     }
 
-//    public static void writeToCSV(Example example) {
-//        Path path = new Path.get(FILE_PATH);
-//        if (!Files.exists(path)) {
-//            try {
-//                path = Files.createFile(path);
-//            }catch (IOException ex) {
-//                ex.printStackTrace();
-//            }
-//        }
-//        try {
-//            Files.write(path, Integer.toString(id).getBytes(), new)
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
-//    }
+    public static boolean writeToFile(Example example, String pathToFile) {
+        try(FileChannel channel = FileChannel.open(Paths.get(pathToFile),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.APPEND)){
+            byte[] record = example.toString().getBytes();
+            ByteBuffer buffer = ByteBuffer.allocate(record.length);
+            buffer.put(record);
+            buffer.flip();
+            channel.write(buffer);
+            channel.close();
+            return true;
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+            return false;
+        }
+    }
 
+    /**
+     *
+     * @param pathToFile The path to the file
+     * @return
+     */
+    public static Optional<Example[]> readFromFile(String pathToFile) {
+        Optional<String[]> records = getRecordsFromFile(pathToFile);
+        if (records.isPresent()) {
+            Example[] examples = new Example[records.get().length];
+            for(int i = 0; i < records.get().length; ++i) {
+                String[] fields = records.get()[i].split(",");
+                try {
+                    if (fields.length != NUMBER_OF_FIELDS) {
+                        throw new IllegalArgumentException();
+                    }
+                    examples[i] = new Example(
+                            Integer.parseInt(fields[ID_INDEX]),
+                            Float.parseFloat(fields[DATA_INDEX]),
+                            fields[NAME_INDEX],
+                            fields[SYMBOL_INDEX].charAt(0));
+                } catch (IllegalArgumentException ex) {
+                    System.err.printf("Invalid record: line %d", i + 1);
+                }
+            }
+            return Optional.of(examples);
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<String[]> getRecordsFromFile(String pathToFile) {
+
+        try(FileChannel channel =
+                    FileChannel.open(Paths.get(pathToFile),
+                            StandardOpenOption.READ)) {
+            StringBuilder fileContents = new StringBuilder();
+            ByteBuffer buffer = ByteBuffer.allocate(1);
+            while (channel.read(buffer) != -1) {
+                buffer.flip();
+                fileContents.append((char) buffer.get());
+                buffer.clear();
+            }
+            return Optional.of(fileContents.toString().split("\n"));
+        } catch (IOException ex) {
+            return Optional.empty();
+        }
+    }
     @Override
     public String toString() {
-        return String.format("id=%08d,data=%8.4f,name=%15s,symbol=%c", id, data, name, symbol);
+        return String.format("%d,%f,%s,%c%n", id, data, name, symbol);
     }
 
 }
