73b7694dfba51e10b42696fddadad71df91c41ff
Dan Johnson <jdjohnson8883@gmail.com>
2023-03-21 20:12:59 -0400

    Starting work on multi-project architecture

--- core/src/main/java/edu/cofc/johnson/utils/BinarySerializationEngine.java
+++ core/src/main/java/edu/cofc/johnson/utils/BinarySerializationEngine.java
@@ -13,20 +13,22 @@ import java.nio.file.Paths;
  * This class holds static methods to serialize and deserialize
  * objects in files.
  * </p>
+ *
  * @author James Daniel Johnson
  */
 public class BinarySerializationEngine {
     /**
      * <p>
-     *     Serializes an object to a file.  The object must implement the <code>Serializable</code>
-     *     interface to be stored.
+     * Serializes an object to a file.  The object must implement the <code>Serializable</code>
+     * interface to be stored.
      * </p>
+     *
      * @param serializableObject The object to serialize
-     * @param outputFileName The name of the file to serialize this object to
+     * @param outputFileName     The name of the file to serialize this object to
      * @throws IOException An <code>IOException</code> will be thrown if there is trouble opening a file
      */
     public static void serializeToBinary(Serializable serializableObject,
-                                         String outputFileName) throws IOException{
+                                         String outputFileName) throws IOException {
         Path outputFilePath = Paths.get(outputFileName);
         ObjectOutputStream outputStream = new ObjectOutputStream(Files.newOutputStream(outputFilePath));
         outputStream.writeObject(serializableObject);
@@ -35,25 +37,26 @@ public class BinarySerializationEngine {
 
     /**
      * <p>
-     *     Deserializes an object from the given binary file. As the method returns an <code>Object</code>
-     *     instance, the surrounding code must cast the return value to the proper type. For instance, if the
-     *     class signature for <code>Example</code> is:
-     *     <pre><code>
+     * Deserializes an object from the given binary file. As the method returns an <code>Object</code>
+     * instance, the surrounding code must cast the return value to the proper type. For instance, if the
+     * class signature for <code>Example</code> is:
+     * <pre><code>
      *         public class Example implements Serializable
      *     </code></pre>
-     *     then the calling code should look like:
-     *     <pre><code>
+     * then the calling code should look like:
+     * <pre><code>
      *         Example example = (Example) BinarySerializationEngine.deserializeFromBinary(exampleBinaryFileName);
      *     </code></pre>
-     *     to return the proper type
+     * to return the proper type
      * </p>
+     *
      * @param inputFileName The binary file from which to read the instance from
      * @return The deserialized <code>Object</code> type
-     * @throws IOException if there is a problem opening the file
+     * @throws IOException            if there is a problem opening the file
      * @throws ClassNotFoundException if the definition for the class can not be found
      */
     public static Object deserializeFromBinary(String inputFileName)
-        throws IOException, ClassNotFoundException {
+            throws IOException, ClassNotFoundException {
         Path inputFilePath = Paths.get(inputFileName);
         ObjectInputStream inputStream = new ObjectInputStream(Files.newInputStream(inputFilePath));
         Object object = inputStream.readObject();
