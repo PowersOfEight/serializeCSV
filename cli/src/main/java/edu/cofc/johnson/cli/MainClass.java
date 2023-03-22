56061612166febe8decbed84906efbc2088ee0a7
Dan Johnson <jdjohnson8883@gmail.com>
2023-03-22 14:19:32 -0400

    Added a command line subproject

--- cli/src/main/java/edu/cofc/johnson/cli/MainClass.java
+++ cli/src/main/java/edu/cofc/johnson/cli/MainClass.java
@@ -3,6 +3,7 @@ package edu.cofc.johnson.cli;
 import edu.cofc.johnson.core.example.Example;
 
 public class MainClass {
+    // This is just to make sure the core and the CLI can communicate for now
     public static void main(String[] args) {
         Example example = new Example(13, 34.56f, "Hello", 'A');
         System.out.printf("The current example: %s\n", example.printToCSVRecord());
